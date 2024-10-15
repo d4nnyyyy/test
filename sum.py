fbb0524f6090361a3791d5d64b7bf979a6e82d79
Nikhil <nikhil59jj@gmail.com>
2024-10-15 11:17:52 +0530

    updated python file

--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
 #program to add two numbers
-a=int(input("enter a number"))
-b=int(input("enter a number"))
+a=int(input("enter a number "))
+b=int(input("enter a number "))
 print("sum of the two  number is  ",a+b)
